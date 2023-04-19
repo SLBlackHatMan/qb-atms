7a9c06e87ba1d5bf547178ba63de5b3b34b73539
Blacka On Live <68303323+SLBlackHatMan@users.noreply.github.com>
2023-04-19 21:54:28 +0530

    Config ATM Coords
    add all locations in map

--- client/main.lua
+++ client/main.lua
@@ -1,7 +1,5 @@
 local QBCore = exports['qb-core']:GetCoreObject()
-
 -- Functions
-
 local function PlayATMAnimation(animation)
     local playerPed = PlayerPedId()
     if animation == 'enter' then
@@ -11,7 +9,6 @@ local function PlayATMAnimation(animation)
         end
         TaskPlayAnim(playerPed, 'amb@prop_human_atm@male@enter', "enter", 1.0,-1.0, 3000, 1, 1, true, true, true)
     end
-
     if animation == 'exit' then
         RequestAnimDict('amb@prop_human_atm@male@exit')
         while not HasAnimDictLoaded('amb@prop_human_atm@male@exit') do
@@ -21,15 +18,31 @@ local function PlayATMAnimation(animation)
     end
 end
 
+local listen = false
+local function Listen4Control()
+    CreateThread(function()
+        listen = true
+        while listen do
+            if IsControlJustPressed(0, 38) then -- E
+                    exports["qb-core"]:KeyPressed()
+                    TriggerServerEvent("qb-atms:server:enteratm")
+                listen = false
+                break
+            end
+            Wait(1)
+        end
+    end)
+end
+
 -- Events
 
 RegisterNetEvent("hidemenu", function()
+	InATM = false
     SetNuiFocus(false, false)
     SendNUIMessage({
         status = "closeATM"
     })
 end)
-
 RegisterNetEvent('qb-atms:client:updateBankInformation', function(banking)
     SendNUIMessage({
         status = "loadBankAccount",
@@ -53,6 +66,41 @@ if Config.UseTarget then
         })
     end)
 end
+local NewZones = {}
+CreateThread(function()
+    if not Config.UseTarget then
+        for k, v in pairs(Config.ATMcoords) do
+            NewZones[#NewZones+1] = CircleZone:Create(vector3(v.x, v.y, v.z), 1.5, {
+                useZ = true,
+                debugPoly = false,
+                name = k,
+            })
+        end
+
+        local combo = ComboZone:Create(NewZones, {name = "RandomZOneName", debugPoly = false})
+        combo:onPlayerInOut(function(isPointInside, point, zone)
+            if isPointInside then
+                exports["qb-core"]:DrawText("[E] Use ATM")
+                Listen4Control()
+            else
+                exports["qb-core"]:HideText()
+                listen = false
+            end
+        end)
+	else
+		exports['qb-target']:AddTargetModel(Config.ATMModels, {
+				options = {
+					{
+						event = 'qb-atms:server:enteratm',
+						type = 'server',
+						icon = "fas fa-credit-card",
+						label = "Use ATM",
+					},
+				},
+				distance = 1.5,
+		})
+	end
+end)
 
 RegisterNetEvent('qb-atms:client:loadATM', function(cards)
     if cards and cards[1] then
@@ -83,17 +131,17 @@ RegisterNetEvent('qb-atms:client:loadATM', function(cards)
         QBCore.Functions.Notify("Please visit a branch to order a card", "error")
     end
 end)
-
 -- Callbacks
 
 RegisterNUICallback("NUIFocusOff", function()
+	InATM = false
+
     SetNuiFocus(false, false)
     SendNUIMessage({
         status = "closeATM"
     })
     PlayATMAnimation('exit')
 end)
-
 RegisterNUICallback("playATMAnim", function()
     local anim = 'amb@prop_human_atm@male@idle_a'
     RequestAnimDict(anim)
@@ -102,13 +150,11 @@ RegisterNUICallback("playATMAnim", function()
     end
     TaskPlayAnim(PlayerPedId(), anim, "idle_a", 1.0,-1.0, 3000, 1, 1, true, true, true)
 end)
-
 RegisterNUICallback("doATMWithdraw", function(data)
     if data then
         TriggerServerEvent('qb-atms:server:doAccountWithdraw', data)
     end
 end)
-
 RegisterNUICallback("loadBankingAccount", function(data)
     QBCore.Functions.TriggerCallback('qb-atms:server:loadBankAccount', function(banking)
         if banking and type(banking) == "table" then
@@ -124,7 +170,6 @@ RegisterNUICallback("loadBankingAccount", function(data)
         end
     end, data.cid, data.cardnumber)
 end)
-
 RegisterNUICallback("removeCard", function(data)
     QBCore.Functions.TriggerCallback('qb-debitcard:server:deleteCard', function(hasDeleted)
         if hasDeleted then
